c62db50a72f64f2ef5738c889b4a43587cb1fe62
RanzzNation <116924759+RanzzNation@users.noreply.github.com>
2022-10-31 18:58:24 +0700

    Update exp-unreg.js

--- plugins/exp-unreg.js
+++ plugins/exp-unreg.js
@@ -1,16 +1,27 @@
 import { createHash } from 'crypto'
+
 let handler = async function (m, { args }) {
-  if (!args[0]) throw 'Serial Number kosong'
+
+  if (!args[0]) throw '📮Serial Number kosong'
+
   let user = global.db.data.users[m.sender]
+
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw 'Serial Number salah'
+
+  if (args[0] !== sn) throw '🚫Serial Number salah'
+
   user.registered = false
-  m.reply('```Succes Unreg !```')
+
+  m.reply('📛Kamu Berhasil keluar dari database\n\nᴹᴿ᭄ King Of Bear ×፝֟͜×')
+
 }
+
 handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
+
 handler.tags = ['xp']
 
 handler.command = /^unreg(ister)?$/i
+
 handler.register = true
 
 export default handler
