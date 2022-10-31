68fd63d7205b64d1dcda44984af6d060ed2c3a34
RanzzNation <116924759+RanzzNation@users.noreply.github.com>
2022-10-31 14:39:34 +0700

    Update internet-brainly.js

--- plugins/internet-brainly.js
+++ plugins/internet-brainly.js
@@ -1,30 +1,25 @@
-import * as baileys from '@adiwajshing/baileys'
-import { Brainly } from 'brainly-scraper-v2'
-import fetch from 'node-fetch'
-let brainly = new Brainly('id')
+let fetch = require('node-fetch')
 
-let handler = async (m, { conn, text }) => {
-	if (!text) throw 'Input Query'
-	let res = await brainly.search(text, 'id').catch(() => null)
-	console.log(res)
-	if (res) {
-		let answer = res.map(({ question, answers }, i) => `
-*Pertanyaan*${question.grade ? ` (${question.grade})` : ''}\n${question.content}${answers.map((v, i) => `
-*Jawaban Ke ${i + 1}*${v.verification ? ' (Verified)' : ''}${v.isBest ? ' (Best)' : ''}
-${v.content}${v.attachments.length > 0 ? `\n*Media Url*: ${v.attachments.join(', ')}` : ''}`).join``}`).join('\n' + '-'.repeat(45))
-		m.reply(answer.trim())
-	} else {
-		let answer = await (await fetch(API('violetics', '/api/media/brainly', { query: text }, 'apikey'))).json()
-		answer = answer.result
-		if (!answer.length) throw 'Not found'
-		for (let x = 0; x < answer.length; x++) {
-			await m.reply(`*${answer[x].pertanyaan}*\n_${answer[x].source}_\n${answer[x].jawaban}`)
-			await baileys.delay(2000)
-		}
-	}
+let handler = async (m, { text, command, usedPrefix }) => {
+
+    if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} membaca`
+
+    let res = await fetch(`https://api.xteam.xyz/brainly?soal=${text}&APIKEY=cristian9407`)
+
+    if (!res.ok) throw eror
+
+    let json = await res.json()
+
+    if (!json.status) throw json
+
+    m.reply(json.jawaban)
+
 }
-handler.help = handler.alias = ['brainly']
-handler.tags = ['tools']
-handler.command = /^(brainly)$/i
-handler.limit = true
-export default handler
+
+handler.help = ['Brainly <teks>']
+
+handler.tags = ['internet']
+
+handler.command = /^brainly$/i
+
+module.exports = handler
