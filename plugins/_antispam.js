5dc5361b60a0ca9733954bf6829ab53b560d0160
RanzzNation <116924759+RanzzNation@users.noreply.github.com>
2022-11-02 14:54:32 +0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -8,6 +8,9 @@ export async function all(m) {
             if (this.spam[m.sender].count > 5) {
                 //global.db.data.users[m.sender].banned = true
                 m.reply('*⌦ kamu terdeteksi spam\n*Beri jeda 5 detik* !*')
+                let stiker = await sticker(null, global.API(`https://telegra.ph/file/ae07de02caa0823ff44d9.png`), global.packname, global.author)
+
+ conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, { fileLength: 100, contextInfo: {
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
